b20ef564a65684678df734b12495b826706dde54
xSurfuel2 <xSurfuel2@gmail.com>
2025-09-19 13:31:03 +0200

    v1.3

--- Interfaz - DI/src/VentanaPrincipal/VentanaPrincipalNotas.java
+++ Interfaz - DI/src/VentanaPrincipal/VentanaPrincipalNotas.java
@@ -6,4 +6,5 @@ public class VentanaPrincipalNotas {
     private JRadioButton radioButton1;
     private JButton usernameButton;
     private JButton passwordButton;
+    private JButton button1;
 }
