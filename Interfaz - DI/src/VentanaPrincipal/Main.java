c6bb673d5018d45a3886bef258e689da673fb550
xSurfuel2 <xSurfuel2@gmail.com>
2025-09-19 11:38:33 +0200

    mic

---
+++ Interfaz - DI/src/VentanaPrincipal/Main.java
@@ -0,0 +1,10 @@
+package VentanaPrincipal;
+
+
+public class Main {
+    public static void main(String[] args)
+    {
+
+
+    }
+}
---
+++ Interfaz - DI/src/VentanaPrincipal/VentanaPrincipalNotas.java
@@ -0,0 +1,9 @@
+package VentanaPrincipal;
+
+import javax.swing.*;
+
+public class VentanaPrincipalNotas {
+    private JRadioButton radioButton1;
+    private JButton button1;
+    private JButton button2;
+}
